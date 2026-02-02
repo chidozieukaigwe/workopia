fff3e7eab75fd09c8724355ae25ea3d3835c5e59
chido <chidodesigns@gmail.com>
2026-02-02 20:38:52 +0000

    alpine js intro and optional fields display in input cmps

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,9 +1,10 @@
 @props([
     'type',
     'message',
+    'timeout' => 5000,
 ])
 @if(session()->has($type))
-    <div class="mb-4 p-4 text-sm text-white rounded {{ $type == 'success' ? 'bg-green-500' : 'bg-red-500'}}">
+    <div x-data="{ show: true}" x-init="setTimeout(() => show = false, {{ $timeout }})" x-show="show"  class="mb-4 p-4 text-sm text-white rounded {{ $type == 'success' ? 'bg-green-500' : 'bg-red-500'}}">
         {{ $message }}
     </div>
 @endif
