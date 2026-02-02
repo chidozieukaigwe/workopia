cc4301d41e638cdc350f0a52d0b18b7027dd62b1
chido <chidodesigns@gmail.com>
2026-02-02 08:47:36 +0000

    hero component added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,15 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if(request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
