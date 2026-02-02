70435f0bed2d28fdccf79a4d4e10f48b492abd39
chido <chidodesigns@gmail.com>
2026-02-02 21:55:23 +0000

    update a job controller method and views added

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,6 +6,7 @@ use App\Models\Job;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 
 class JobController extends Controller
 {
@@ -32,6 +33,8 @@ class JobController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+
+        
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
@@ -46,14 +49,24 @@ class JobController extends Controller
             'state'=> 'required|string',
             'zipcode'=> 'nullable|string',
             'contact_email' => 'required|string',
+            'contact_phone'=> 'nullable|string',
             'company_name'=> 'required|string',
             'company_description'=> 'nullable|string',
             'company_logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'company_website'=> 'nullable|url',
         ]);
 
+
         // Hardcoded user ID
         $validatedData['user_id'] = 1;
+
+        // Check for image 
+        if($request->hasFile('company_logo')){
+            $path = $request->file('company_logo')->store('logos', 'public');
+
+            $validatedData['company_logo'] = $path;
+
+        }
 
         Job::create($validatedData);
 
@@ -71,17 +84,52 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id): string
+    public function edit(Job $job): View
     {
-        return 'Edit';
+        return view('jobs.edit')->with('job', $job);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): string
+    public function update(Request $request, Job $job): RedirectResponse
     {
-        return 'Update';
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'salary' => 'required|integer',
+            'tags' => 'nullable|string',
+            'job_type'=> 'required|string',
+            'remote'=> 'required|boolean',
+            'requirements'=> 'nullable|string',
+            'benefits'=> 'nullable|string',
+            'address'=> 'nullable|string',
+            'city'=> 'required|string',
+            'state'=> 'required|string',
+            'zipcode'=> 'nullable|string',
+            'contact_email' => 'required|string',
+            'contact_phone'=> 'nullable|string',
+            'company_name'=> 'required|string',
+            'company_description'=> 'nullable|string',
+            'company_logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'company_website'=> 'nullable|url',
+        ]);
+        
+
+        // Check for image 
+        if($request->hasFile('company_logo')){
+
+            // Delete old logo 
+            Storage::delete('public/' . basename($job->company_logo));
+
+            $path = $request->file('company_logo')->store('logos', 'public');
+
+            $validatedData['company_logo'] = $path;
+        }
+
+        $job->update($validatedData);
+
+        return redirect()->route('jobs.show', $job)->with('success','Job listing updated successfully.');
     }
 
     /**
--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -13,7 +13,7 @@
 @endif
     <input id="{{ $id }}" type="{{ $type }}" name="{{ $name }}"
         class="w-full px-4 py-2 border rounded focus:outline-none @error('{{ $name }}') border-red-500 @enderror"
-        placeholder="{{ $placeholder }}" value="{{ old($name) }}" />
+        placeholder="{{ $placeholder }}" value="{{ old($name, $value) }}" />
     @error($name)
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
     @enderror
