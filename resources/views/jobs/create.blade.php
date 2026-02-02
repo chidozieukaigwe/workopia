a70dc1d51550cbef0cbc8e6f93cca0b418c78f2d
chido <chidodesigns@gmail.com>
2026-02-02 19:41:26 +0000

    create form refactored to component files

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,24 +1,85 @@
 <x-layout>
     <x-slot name="title">Create Job</x-slot>
-    <h1>Create New Job</h1>
-    <form action="/jobs" method="POST">
-        @csrf
-        <div class="my-5">
-            <label for="title">Job Title:</label>
-            <input type="text" id="title" name="title" required placeholder="title" class="bg-white"
-                value="{{ old('title') }}">
-            @error('title')
-                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
-            @enderror
-        </div>
+    <div class="bg-white mx-auto p-8 rounded-lg shadow-md w-full md:max-w-3xl">
+        <h2 class="text-4xl text-center font-bold mb-4">
+            Create Job Listing
+        </h2>
+        <form method="POST" action="/jobs" enctype="multipart/form-data">
+            @csrf
+            <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
+                Job Info
+            </h2>
 
-        <div class="mb-5">
-            <label for="description">Job Description:</label>
-            <textarea id="description" name="description" required class="bg-white">{{ old('description') }}</textarea>
-            @error('description')
-                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
-            @enderror
-        </div>
-        <button type="submit">Create Job</button>
-    </form>
+            <x-inputs.text id="title" name="title" label="Job Title" placeholder="Senior Software Developer" />
+
+
+            <x-inputs.text-area id="description" name="description" label="Description"
+                placeholder="Job description goes here..." />
+
+
+            <x-inputs.text id="salary" name="salary" label="Salary" placeholder="90000" type="number" />
+
+
+            <x-inputs.text-area id="requirements" name="requirements" label="Requirements"
+                placeholder="Bachelor's degree in Computer Science" />
+
+            <x-inputs.text-area id="benefits" name="benefits" label="Benefits"
+                placeholder="Health insurance, 401k, paid time off" />
+
+            <x-inputs.text id="tags" name="tags" label="Tags (comma-separated)"
+                placeholder="development,coding,java,python" />
+
+
+            <x-inputs.select id="job_type" name="job_type" label="Job Type" value="{{ old('job_type') }}" :options="[
+        'full_time' => 'Full Time',
+        'part_time' => 'Part Time',
+        'contract' => 'Contract',
+        'internship' => 'Internship',
+        'temporary' => 'Temporary',
+        'volunteer' => 'Volunteer',
+        'on-call' => 'On-call',
+    ]" />
+
+            <x-inputs.select id="remote" name="remote" label="Remote" :options="[
+        '0' => 'No',
+        '1' => 'Yes',
+    ]" />
+
+            <x-inputs.text id="address" name="address" label="Address" placeholder="123 Main St" />
+
+            <x-inputs.text id="city" name="city" label="City" placeholder="Albany" />
+
+            <x-inputs.text id="state" name="state" label="State" placeholder="NY" />
+
+            <x-inputs.text id="zipcode" name="zipcode" label="ZIP Code" placeholder="12201" />
+
+            <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
+                Company Info
+            </h2>
+
+            <x-inputs.text id="company_name" name="company_name" label="Company Name"
+                placeholder="Enter Company name" />
+
+            <x-inputs.text-area id="company_description" name="company_description" label="Company Description"
+                placeholder="Company Description" />
+
+            <x-inputs.text id="company_website" name="company_website" label="Company Website"
+                placeholder="Enter website" />
+
+            <x-inputs.text id="contact_phone" name="contact_phone" label="Contact Phone"
+                placeholder="Enter Contact Phone" />
+
+
+            <x-inputs.text id="contact_email" name="contact_email" label="Contact Email"
+                placeholder="Enter Contact Email" type="email" />
+
+
+            <x-inputs.file id="company_logo" name="company_logo" label="Company Logo" />
+
+            <button type="submit"
+                class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
+                Save
+            </button>
+        </form>
+    </div>
 </x-layout>
