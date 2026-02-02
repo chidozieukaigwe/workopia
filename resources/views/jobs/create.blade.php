e9b381e30abd1e5fb06340a14e943dd716b82dfc
chido <chidodesigns@gmail.com>
2026-02-02 15:08:15 +0000

    job - user relationship link

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,12 +3,22 @@
     <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
-        <label for="title">Job Title:</label>
-        <input type="text" id="title" name="title" required placeholder="title">
-        <br><br>
-        <label for="description">Job Description:</label>
-        <textarea id="description" name="description" required></textarea>
-        <br><br>
+        <div class="my-5">
+            <label for="title">Job Title:</label>
+            <input type="text" id="title" name="title" required placeholder="title" class="bg-white"
+                value="{{ old('title') }}">
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-5">
+            <label for="description">Job Description:</label>
+            <textarea id="description" name="description" required class="bg-white">{{ old('description') }}</textarea>
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
         <button type="submit">Create Job</button>
     </form>
 </x-layout>
