27c1d1783338c4210f2eae427d0a685529220ed8
chido <chidodesigns@gmail.com>
2026-02-03 16:26:50 +0000

    search functionality

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -4,16 +4,8 @@
 
 <section class="hero relative bg-cover bg-center bg-no-repeat h-72 flex items-center">
     <div class="overlay"></div>
-    <div class="container mx-auto text-center z-10">
+    <div class="c ontainer mx-auto text-center z-10">
         <h2 class="text-5xl text-white font-bold mb-8">{{ $title }}</h2>
-        <form class="block mx-5 md:mx-auto md:space-x-2">
-            <input type="text" name="keywords" placeholder="Keywords"
-                class="w-full md:w-72 px-4 py-3 focus:outline-none bg-white rounded" />
-            <input type="text" name="location" placeholder="Location"
-                class="w-full md:w-72 px-4 py-3 focus:outline-none bg-white rounded" />
-            <button class="w-full md:w-auto bg-blue-700 hover:bg-blue-600 text-white px-4 py-3 focus:outline-none">
-                <i class="fa fa-search mr-1"></i> Search
-            </button>
-        </form>
+        <x-search/>
     </div>
 </section>
